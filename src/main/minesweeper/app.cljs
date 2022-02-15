a8b4a432cda01a74e1cc9ac2805c5fa6f07c6288
Steven B <steven@botzek.com>
2022-02-14 20:45:56 -0800

    App.cljs has release config

--- src/main/minesweeper/app.cljs
+++ src/main/minesweeper/app.cljs
@@ -1,19 +1,22 @@
 (ns minesweeper.app
-  (:require
-    [minesweeper.core :as core]
-    [cljs.spec.alpha :as s]
-    [expound.alpha :as expound]
-    #_[devtools.core :as devtools]))
+  (:require [minesweeper.core :as core]
+            [cljs.spec.alpha :as s]
+            [expound.alpha :as expound]
+            #_[devtools.core :as devtools]))
 
-(extend-protocol IPrintWithWriter
-  js/Symbol
-  (-pr-writer [sym writer _]
-    (-write writer (str "\"" (.toString sym) "\""))))
+(if goog.DEBUG
+  (do
+    (extend-protocol IPrintWithWriter
+      js/Symbol
+      (-pr-writer [sym writer _]
+        (-write writer (str "\"" (.toString sym) "\""))))
 
-(set! s/*explain-out* expound/printer)
+    (set! s/*explain-out* expound/printer)
 
-(enable-console-print!)
+    (enable-console-print!)
 
-#_(devtools/install!)
-
+    #_(devtools/install!))
+  (do
+    ;;ignore println statements in prod
+    (set! *print-fn* (fn [& _]))))
 (core/init!)
