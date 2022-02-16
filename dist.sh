d3356d8cefdb97957edeeaf4ad8b84891a6459c9
Steven B <steven@botzek.com>
2022-02-15 23:02:55 -0800

    Gzip files

--- dist.sh
+++ dist.sh
@@ -6,5 +6,11 @@ npx shadow-cljs release app
 rsync -av --exclude='js' public/* dist/
 rsync -av --exclude '*.edn' target/release/* dist/
 
+for file in $(find dist -type f)
+do
+	gzip $file
+	touch $file
+done
+
 tar -czvf dist.tar.gz dist
 rm -rf dist
